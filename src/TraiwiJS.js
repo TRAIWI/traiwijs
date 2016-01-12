5dd30be2ffc0aa6e5c14f95bfbc495f673dd7f42
scipper <scipper@myscipper.de>
2016-01-12 13:49:56 +0100

    addded pauseEvent to TraiwiJS

--- src/TraiwiJS.js
+++ src/TraiwiJS.js
@@ -3,3 +3,16 @@
 var TraiwiJS = function() {
 	
 };
+
+TraiwiJS.prototype.pauseEvent = function(event) {
+	if(event.stopPropagation) {
+		event.stopPropagation();
+	}
+	if(event.preventDefault) {
+		event.preventDefault();
+	}
+	event.cancelBubble = true;
+	event.returnValue = false;
+	
+	return false;
+};
