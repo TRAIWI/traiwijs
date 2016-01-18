da1229ed0e86329583376d8dce5cfbf2cf57b481
scipper <scipper@myscipper.de>
2016-01-18 15:43:37 +0100

    added PdfResponser to open pdf in new window

---
+++ src/TraiwiJS.PdfResponser.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var TraiwiJS = TraiwiJS || {};
+
+TraiwiJS.PdfResponser = function(trigger, method, options) {
+	$("#" + trigger).click(function() {
+		if($("#ajax-loading").length == 1) {
+			$("#ajax-loading").dialog("open");
+		}
+		
+		var parameter = options || {};
+
+		$.post(TraiwiJS.prototype.getUrl() + "/" + method + ".do", parameter, function(data) {
+			if($("#ajax-loading").length == 1) {
+				$("#ajax-loading").dialog("close");
+			}
+			
+			if(data == "false") {
+				alert("Die PDF konnte nicht erstellt werden.");
+			} else {
+				if($.browser.device == true) {
+					window.open(data, "_self");
+				} else {
+					var win_popup = window.open(data, "_blank");
+					if(!win_popup) {
+						alert("Pop-Ups werden durch Ihre Browser-Einstellungen blockiert.");
+					}
+				}
+			}
+		});
+	});
+};
